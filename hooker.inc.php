f65fbefed46dfeb0d8cf8d8b67c1c729687d72a3
Magillusionalgame <fantasyoui@gmail.com>
2012-12-05 17:11:28 +0800

    添加嵌入点检查文件和嵌入点后台管理面板

---
+++ hooker.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+        [DISCUZ!] addbybishop/hooker.inc.php - 用正则表达式管理嵌入点和非官方修改
+
+        Version: 0.01
+        Author: Bovvic(671064591@qq.com)
+        Copyright: For author
+        Last Modified: 2012.12.05
+*/
+if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+	exit('Access Denied');
+}
+
+?>
