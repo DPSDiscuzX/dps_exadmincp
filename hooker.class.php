e93ad52f4da04241a2e7aaa2f3ce89bcf965fb67
Magillusionalgame <fantasyoui@gmail.com>
2012-12-05 17:18:56 +0800

    修改邮件地址

--- hooker.class.php
+++ hooker.class.php
@@ -3,7 +3,7 @@
         [DISCUZ!] addbybishop/hooker.class.php - 文件作用描述
 
         Version: 0.01
-        Author: Bovvic(作者e-mail)
+        Author: Bovvic(671064591@qq.com)
         Copyright: For author
         Last Modified: 2012.12.05
 */
