f9df199aa58fc41630fb2a75993b6bce9817c4f6
Magillusionalgame <fantasyoui@gmail.com>
2012-12-05 17:17:14 +0800

    添加

---
+++ hooker.class.php
@@ -0,0 +1,14 @@
+<?php
+/*
+        [DISCUZ!] addbybishop/hooker.class.php - 文件作用描述
+
+        Version: 0.01
+        Author: Bovvic(作者e-mail)
+        Copyright: For author
+        Last Modified: 2012.12.05
+*/
+if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+	exit('Access Denied');
+}
+
+?>
